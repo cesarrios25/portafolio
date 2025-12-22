ad18a687c705e50fc726f21986bf6922e8eedc50
Cesar Rios <cesar.webdev@outlook.com>
2025-12-22 16:43:30 -0500

    portafolio completo y actualizado

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+    hamburger.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        hamburger.classList.toggle('open');
+    });
